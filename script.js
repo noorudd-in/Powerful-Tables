0c8ee6d1bb09c3097934a3f0846bdf138e7483f7
Noor <nooruddin12401@gmail.com>
2023-12-08 19:32:54 +0530

    Revanced the UI with Bootstrap
    - Revanced the whole app and implemented everything with Bootstrap UI
    - Fixed the bug where table was still in edit mode after it is deleted.
    - Changed the alert to modal for better UI experience.
    - Made the table responsive for mobile screens.
    - Added Theme and Role.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let schoolNames = ["Select", "School 1", "School 2", "School 3", "School 4"];
+/* Data for options */
+let schoolNames = [
+  { 0: "Select" },
+  { 12: "School 1" },
+  { 17: "School 2" },
+  { 15: "School 3" },
+];
 let departmentNames = [
   "Select",
   "Electronics",
@@ -8,38 +14,87 @@ let departmentNames = [
   "Civil",
   "Automobile",
 ];
-let courseNames = [
-  "Select",
-  "Subject 1",
-  "Subject 2",
-  "Subject 3",
-  "Subject 4",
-  "Subject 5",
-  "Subject 6",
-  "Subject 7",
-];
+
 let electronicsCourses = [
   "Select",
-  "Electronics 1",
-  "Electronics 2",
-  "Electronics 3",
-  "Electronics 4",
-  "Electronics 5",
-  "Electronics 6",
+  "Analog Communication",
+  "Digital Electronics",
+  "Digital System Design",
+  "Electronic Device & Circuits",
+  "Linear Integrated Circuit",
+  "Discrete Time Signal",
+  "Microcontrollers",
+  "Antenna & Radio Wave",
+  "Optical Communication",
 ];
-let csCourses = ["Select", "CS 1", "CS 2", "CS 3", "CS 4"];
-let itCourses = ["Select", "IT 1", "IT 2", "IT 3"];
-let mechanicalCourses = ["Select", "Mechanical 1", "Mechanical 2"];
-let civilCourses = ["Select", "Civil 1", "Civil 2"];
-let automobileCourses = ["Select", "Automobile 1", "Automobile 2"];
-
-let theme = "light";
+let csCourses = [
+  "Select",
+  "C++",
+  "Data Structures",
+  "Computer Networks",
+  "Discrete Mathematics",
+  "Linux OS",
+  "Database Management",
+  "Operating System",
+  "Programming with Java",
+  "Theory of Computation",
+  "Compiler Design",
+];
+let itCourses = [
+  "Select",
+  "Database Management",
+  "File Structure",
+  "Logic Design",
+  "Operating System",
+  "Programming in C",
+  "Object Oriented using C++",
+  "Data Mining",
+  "RDBMS",
+  "Computer Graphics",
+  "Data Compression",
+];
+let mechanicalCourses = [
+  "Select",
+  "Fluid Mechanics",
+  "Material Science",
+  "Design",
+  "Thermodynamics",
+  "Applied Mechanics",
+  "Solid Mechanics",
+  "Heat Transfer",
+  "Strenght of Material",
+  "Thermal Engineering",
+];
+let civilCourses = [
+  "Select",
+  "Structural Engineering",
+  "Envoirnmental Engineering",
+  "Geotechnical",
+  "Transportation Engineering",
+  "Surveying",
+  "Construction Engineering",
+  "Hydraulic Engineering",
+];
+let automobileCourses = [
+  "Select",
+  "Applied Mechanics",
+  "Mathematics",
+  "Fluid Mechanics",
+  "Vehicle Drawing",
+  "Chassis",
+  "Chemical Engineering",
+  "Automobile Engine",
+  "Robotics",
+  "Solid Mechanics",
+  "Thermodynamics",
+];
 
 // Mapping the dropdown content and injecting in html
 let school = document.getElementById("school");
 schoolNames.forEach((element) => {
+  //console.log();
   let options = document.createElement("option");
-  let optionValue = document.createTextNode(element);
+  let optionValue = document.createTextNode(Object.values(element)[0]);
   options.appendChild(optionValue);
   school.appendChild(options);
 });
@@ -74,6 +129,26 @@ function dynamicCourse(e) {
   }
 }
 
+// Function to toggle the mode between "Light" and "Dark"
+function changeTheme() {
+  if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+    document.documentElement.setAttribute("data-bs-theme", "light");
+    document.getElementById("changeTheme").innerText = "Light";
+  } else {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+    document.getElementById("changeTheme").innerText = "Dark";
+  }
+}
+
+// Function to toggle the role between "Admin" and "User"
+function changeRole() {
+  if (document.getElementById("changeRole").innerText == "Admin") {
+    document.getElementById("changeRole").innerText = "User";
+  } else {
+    document.getElementById("changeRole").innerText = "Admin";
+  }
+}
+
 // Check if the submitted email address is uique!
 function uniqueEmailCheck(data, email) {
   for (let i = 0; i < data.length; i++) {
@@ -88,42 +163,37 @@ function uniqueEmailCheck(data, email) {
 // Submitting the form
 function formSubmit(e) {
   e.preventDefault();
-  let name = document.getElementById("fullname").value;
-  let dob = document.getElementById("dob").value;
-  let sex = document.querySelector("input[name='sex']:checked").value;
-  let email = document.getElementById("email").value;
-  let address = document.getElementById("address").value;
-  let school = document.getElementById("school").value;
-  let department = document.getElementById("department").value;
-  let course = document.getElementById("course").value;
-  let number = document.getElementById("number").value;
-  let form = document.querySelector("form");
-
+  const form = document.querySelector("form");
+  const d = new FormData(form);
+  const data = Object.fromEntries(new FormData(form).entries());
   // Form Validation
   let validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!email.match(validRegex)) {
+  if (!data.email.match(validRegex)) {
     alert("Please enter a valid email address!");
-  } else if (school == "Select") {
+  } else if (data.school == "Select") {
     alert("Please select the school");
-  } else if (department == "Select") {
+  } else if (data.department == "Select") {
     alert("Please select the department");
-  } else if (course == "Select") {
+  } else if (data.course == "Select") {
     alert("Please select the course");
-  } else if (number.length < 10 || Number.isFinite(parseInt(number)) == false) {
+  } else if (
+    data.number.length < 10 ||
+    Number.isFinite(parseInt(data.number)) == false
+  ) {
     alert("Please enter 10 digit mobile number");
   } else {
-    let formData = {
-      fullname: name,
-      dob: dob,
-      sex: sex,
-      email: email,
-      address: address,
-      school: school,
-      department: department,
-      course: course,
-      number: number,
-    };
+    let formData = ({
+      fullname,
+      dob,
+      sex,
+      email,
+      address,
+      school,
+      department,
+      course,
+      number,
+    } = data);
     form.reset();
     if (localStorage.getItem("editIndex") == null) {
       // Form data for creating new row
@@ -159,68 +229,57 @@ function formSubmit(e) {
   }
 }
 
+// Create Table whenever called
+function createTable(data = JSON.parse(localStorage["data"])) {
+  let table = document.getElementById("tablebody");
+  let role;
+  if ((document.getElementById("changeRole").innerText = "Admin")) {
+    role = 1;
+  } else {
+    role = 0;
+  }
+  table.innerHTML = ``;
+  data.map((e, index) => {
+    let row = `<tr id="${index}">
+  <td>${JSON.parse(e).fullname}</td>
+  <td>${JSON.parse(e).dob}</td>
+  <td>${JSON.parse(e).sex}</td>
+  <td>${JSON.parse(e).email}</td>
+  <td>${JSON.parse(e).address}</td>
+  <td>${JSON.parse(e).school}</td>
+  <td>${JSON.parse(e).department}</td>
+  <td>${JSON.parse(e).course}</td>
+  <td>${JSON.parse(e).number}</td>
+  ${
+    role == 1
+      ? `<td><button class="btn btn-primary" onclick="editData(${index})">Edit</button></td>
+  <th><input type="checkbox" onChange="bulkDeleteArray(${index}, event)"/></th></tr>`
+      : `</tr>`
+  }
+  `;
+    table.innerHTML += row;
+  });
+}
+
+// Confirm before resetting the form
 function resetConfirm() {
   return confirm("Are you sure you want to reset the form?");
 }
 
-// Create Table whenever called
-function createTable(data = JSON.parse(localStorage["data"])) {
-  let table = document.getElementById("tablebody");
-  let role = document.getElementById("role").value;
-  table.innerHTML = ``;
-  data.map((e, index) => {
-    let row = `<tr id="${index}">
-<td>${JSON.parse(e).fullname}</td>
-<td>${JSON.parse(e).dob}</td>
-<td>${JSON.parse(e).sex}</td>
-<td>${JSON.parse(e).email}</td>
-<td>${JSON.parse(e).address}</td>
-<td>${JSON.parse(e).school}</td>
-<td>${JSON.parse(e).department}</td>
-<td>${JSON.parse(e).course}</td>
-<td>${JSON.parse(e).number}</td>
-${
-  role == 1
-    ? `<td><button onclick="editData(${index})">Edit</button></td>
-<th><input type="checkbox" onChange="bulkDeleteArray(${index}, event)"/></th></tr>`
-    : `</tr>`
-}
-`;
-    table.innerHTML += row;
-  });
-  theme = document.getElementById("theme").value;
-  const cellsth = document.querySelectorAll("table th");
-  cellsth.forEach((cell) => {
-    cell.style.border =
-      theme == "light" ? "1px solid black" : "1px solid white";
-  });
-  const cellstd = document.querySelectorAll("table td");
-  cellstd.forEach((cell) => {
-    cell.style.border =
-      theme == "light" ? "1px solid black" : "1px solid white";
-  });
-}
-
-// Delete all rows
-function deleteAll() {
-  if (window.confirm("Alert! This will DELETE ALL DATA. Are your sure?")) {
-    localStorage.clear();
-    window.location.reload();
-  }
-}
-
 // Prefill the data when edit button is clicked!
 function editData(index) {
-  theme = document.getElementById("theme").value;
+  //theme = document.getElementById("theme").value;
   localStorage.setItem("editIndex", index);
   document.getElementById("submitBtn").innerText = "Update";
-  document.getElementById(`${index}`).style.background =
-    theme == "light" ? "aqua" : "darkslateblue";
+  document.getElementById(`${index}`).style.background = "aqua";
+  //theme == "light" ? "aqua" : "darkslateblue";
   let data = JSON.parse(localStorage["data"]);
   let dataObj = JSON.parse(data[index]);
   document.getElementById("department").value = dataObj.department;
   let selected = document.getElementById("department").value;
   let arr = eval(`${selected.toLowerCase()}Courses`);
+  let course = document.getElementById("course");
+  course.innerHTML = ``;
   arr.forEach((element) => {
     let options = document.createElement("option");
     let optionValue = document.createTextNode(element);
@@ -240,11 +299,11 @@ function editData(index) {
 // Bulk Delete
 let deletingIndex = new Set();
 function bulkDeleteArray(index, event) {
-  theme = document.getElementById("theme").value;
+  //theme = document.getElementById("theme").value;
   if (event.target.checked) {
     deletingIndex.add(index);
-    document.getElementById(`${index}`).style.background =
-      theme == "light" ? "indianred" : "firebrick";
+    document.getElementById(`${index}`).style.background = "indianred";
+    //theme == "light" ? "indianred" : "firebrick";
   } else {
     deletingIndex.delete(index);
     document.getElementById(`${index}`).style.background = "";
@@ -277,25 +336,21 @@ function bulkDelete() {
 }
 
 // Export Table data to pdf or excel sheet.
-function exportData() {
-  let method = document.getElementById("exportData").value;
+function exportData(method) {
   if (method == "pdf") {
-    document.getElementById("form").style.display = "none";
-    document.getElementById("features").style.display = "none";
+    //document.getElementById("form").style.display = "none";
+    //document.getElementById("features").style.display = "none";
     window.print();
-    document.getElementById("exportData").value = "select";
+
     document.getElementById("form").style.display = "";
     document.getElementById("features").style.display = "";
   } else if (method == "xlsx") {
     let table = document.getElementById("table");
     const wb = XLSX.utils.table_to_book(table, { sheet: "sheet-1" });
     XLSX.writeFile(wb, "Sheet.xlsx");
-    document.getElementById("exportData").value = "select";
   } else if (method == "csv") {
     exportCSV();
-    document.getElementById("exportData").value = "select";
   } else if (method == "json") {
-    document.getElementById("exportData").value = "select";
     exportJSON();
   }
 }
@@ -352,8 +407,7 @@ function versionHistory() {
 }
 
 // Render past version of table data
-function renderVersion() {
-  let version = document.getElementById("versionHistory").value;
+function renderVersion(version) {
   let data = localStorage.getItem("data");
   let data_1 = localStorage.getItem("data_1");
   let data_2 = localStorage.getItem("data_2");
@@ -394,44 +448,6 @@ function renderVersion() {
     localStorage.setItem("data_1", data);
     localStorage.setItem("data", temp);
     createTable();
-  } else {
-    document.getElementById("versionHistory").value = "select";
-    return;
-  }
-  document.getElementById("versionHistory").value = "select";
-}
-
-//Change theme based on the user selection
-function changeTheme() {
-  let theme = document.getElementById("theme").value;
-  document.body.style.background = theme == "light" ? "#f4f2ee" : "black";
-  document.body.style.color = theme == "light" ? "#000000" : "#f4f2ee";
-  const cellsth = document.querySelectorAll("table th");
-  cellsth.forEach((cell) => {
-    cell.style.border =
-      theme == "light" ? "1px solid black" : "1px solid white";
-  });
-  const cellstd = document.querySelectorAll("table td");
-  cellstd.forEach((cell) => {
-    cell.style.border =
-      theme == "light" ? "1px solid black" : "1px solid white";
-  });
-}
-
-// Render table based on the current role.
-function currentRole() {
-  createTable();
-  let role = document.getElementById("role").value;
-  if (role == 0) {
-    document.getElementById("editOnTable").style.display = "none";
-    document.getElementById("deleteOnTable").style.display = "none";
-    document.getElementById("importFeaure").style.display = "none";
-    document.getElementById("deleteAll").style.display = "none";
-  } else {
-    document.getElementById("editOnTable").style.display = "";
-    document.getElementById("deleteOnTable").style.display = "";
-    document.getElementById("importFeaure").style.display = "";
-    document.getElementById("deleteAll").style.display = "";
   }
 }
 
@@ -476,6 +492,9 @@ function sorting(e, name) {
 function exportCSV() {
   let data = JSON.parse(localStorage.getItem("data"));
   let fileContent = "";
+  let headers =
+    "Full Name;Email;Date of Birth;Mobile Number;Sex;Address;School;Department;Course";
+  fileContent += headers + "\n";
   data.map((e) => {
     let result = JSON.parse(e);
     let str = "";
@@ -521,5 +540,17 @@ function toggleSorting(e) {
   }
 }
 
+function firstLoad() {
+  if (localStorage.getItem("data") == null) {
+    let preData =
+      '["{\\"fullname\\":\\"Nooruddin Shaikh\\",\\"email\\":\\"hi@noorudd.in\\",\\"dob\\":\\"2000-01-01\\",\\"number\\":\\"9876543210\\",\\"sex\\":\\"male\\",\\"address\\":\\"Mumbai, Maharashtra, India\\",\\"school\\":\\"School 1\\",\\"department\\":\\"Electronics\\",\\"course\\":\\"Digital System Design\\"}"]';
+    localStorage.setItem("data", preData);
+    localStorage.setItem("data_1", JSON.stringify([]));
+    localStorage.setItem("data_2", JSON.stringify([]));
+    localStorage.setItem("data_3", JSON.stringify([]));
+  }
+  createTable();
+}
+
 // Load the data if exist when page loads
-document.onload = createTable();
+document.onload = firstLoad();
